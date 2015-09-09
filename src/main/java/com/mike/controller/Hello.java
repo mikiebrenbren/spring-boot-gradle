3a8260552bb134d11bd2ac8ff8cf8e8b7f1352ad
Michael Brennan <Michael Brennan>
2015-09-08 22:35:14 -0600

    added a name attribute to model object, and name in the request parameter

--- src/main/java/com/mike/controller/Hello.java
+++ src/main/java/com/mike/controller/Hello.java
@@ -3,6 +3,7 @@ package com.mike.controller;
 import com.mike.model.ResponseModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Created by michaelbrennan on 9/8/15.
+ * Created by Michael Brennan on 9/8/15.
  */
 @RestController
 @RequestMapping("/")
 public class Hello {
 
 
-    @RequestMapping
-    public ResponseEntity<ResponseModel> helloWorld(HttpServletRequest request, HttpServletResponse response){
+    @RequestMapping("hello/{name}")
+    public ResponseEntity<ResponseModel> helloWorld(HttpServletRequest request, HttpServletResponse response, @PathVariable final String name){
 
-        return new ResponseEntity<>(new ResponseModel(request), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseModel(name, request), HttpStatus.OK);
     }
 }
