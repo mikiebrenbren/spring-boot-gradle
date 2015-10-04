b88b88f9e6cbc9b46f15a147098f317081763648
Mike Brennan <mikiebrenbren@gmail.com>
2015-10-03 20:47:17 -0600

    good login/logout flow with java config

--- src/main/java/com/mike/config/SecurityConfig.java
+++ src/main/java/com/mike/config/SecurityConfig.java
@@ -1,18 +1,41 @@
 package com.mike.config;
 
+import com.mike.handler.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private CustomAuthenticationSuccessHandler successHandler;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+//                .loginPage("/login")
+//                .failureUrl("/login")
+                .successHandler(successHandler) // autowired or defined below
+                .and()
+                .logout()
+                .permitAll()
+                .and();
     }
 }
