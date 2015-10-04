94013b95528dc52c26fe20d7fd06f6d877d7eaa8
Mike Brennan <mikiebrenbren@gmail.com>
2015-10-03 22:03:57 -0600

    using vanilla spring security for now

--- src/main/java/com/mike/Application.java
+++ src/main/java/com/mike/Application.java
@@ -1,12 +1,13 @@
 package com.mike;
 
-import com.mike.config.SecurityConfig;
+import org.apache.catalina.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 @SpringBootApplication
 public class Application extends AbstractSecurityWebApplicationInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/mike/config/SecurityConfig.java
+++ src/main/java/com/mike/config/SecurityConfig.java
@@ -2,21 +2,19 @@ package com.mike.config;
 
 import com.mike.handler.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
-@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomAuthenticationSuccessHandler successHandler;
-//    @Autowired
-//    private
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
